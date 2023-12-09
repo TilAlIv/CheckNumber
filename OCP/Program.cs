f69cd0525ef9965dd2a26c3785ad44db86a5d320
Alex Til <til.a@abitech.kz>
2023-12-09 22:28:03 +0600

    добавил проекты

---
+++ OCP/Program.cs
@@ -0,0 +1,15 @@
+﻿using OCP;
+using Settings;
+
+var root = Startup.GetAppConfiguration();
+var count = Convert.ToInt32(root["Count"]);
+int rangeNumbers = Convert.ToInt32(root["RangeNumbers"]);
+
+Random rnd = new Random();
+var searchNumber = rnd.Next(0, rangeNumbers);
+
+// Согласно принципу OCP, сделал реализацию интерфейса IGame.
+StartGame gm = new StartGame(new SearchNumberGame());
+gm.Start(count, searchNumber); 
+
+
---
+++ SRP/Program.cs
@@ -0,0 +1,12 @@
+﻿using Settings;
+using SRP;
+
+var root = Startup.GetAppConfiguration(); // Из стат. класса получаем конфигурацию, где задаются попытки и диапазон.
+var count = Convert.ToInt32(root["Count"]);
+int rangeNumbers = Convert.ToInt32(root["RangeNumbers"]);
+
+Random rnd = new Random();
+var searchNumber = rnd.Next(0, rangeNumbers);
+
+Game game = new Game(); // Согласно принципу SRP, логику связанную с игровым процессом вынес в отдельный класс.
+game.Start(count, searchNumber);
